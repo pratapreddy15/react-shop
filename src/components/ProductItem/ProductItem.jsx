0f91b94b90d12f05402d05535e554157ee8df074
Pratap Reddy <pratapreddy15@gmail.com>
2021-05-26 06:46:42 +1000

    added quantity input and add or remove quantity

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -4,16 +4,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Card from '../UI/Card/Card';
+import QuantityInput from '../UI/QuantityInput/QuantityInput';
 
 const ProductItem = (props) => {
   const { product } = props;
-  console.log(product);
   return (
     <Card>
       <div className={classes.productImage}>
         <img src={product.image} />
       </div>
       <div className={classes.productTitle}>{product.title}</div>
+      <QuantityInput />
     </Card>
   );
 };
